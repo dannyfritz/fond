77ba138e6f1b5d52dfaef4e6e1624c6c1dc0e3ee
Danny Fritz <dannyfritz@gmail.com>
2016-08-21 17:56:00 -0600

    🎉 Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import Runtime from "./lib/Runtime"
+import Graphics from "./lib/Graphics"
+import Keyboard from "./lib/Keyboard"
+import Timer from "./lib/Timer"
+import Debug from "./lib/Debug"
+
+export default new Runtime()
+export { Runtime, Graphics, Keyboard, Timer, Debug }
