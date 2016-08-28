1ce63f2c162ce16e69841f88194c067cd5de4bcd
Danny Fritz <dannyfritz@gmail.com>
2016-08-27 23:23:16 -0600

    ✨ add error to fond-debug

--- packages/fond-debug/lib/index.js
+++ packages/fond-debug/lib/index.js
@@ -4,14 +4,20 @@ const DEBUG = process.env.NODE_ENV === "development"
 
 //eslint-disable-next-line brace-style
 let assert = () => {}
-//eslint-disable-next-line brace-style
-let log = () => {}
-
 if (DEBUG)
 {
   assert = nodeAssert
+}
+
+//eslint-disable-next-line brace-style
+let log = () => {}
+if (DEBUG)
+{
   // eslint-disable-next-line no-console
   log = console.log
 }
 
-export default { assert, log, DEBUG }
+// eslint-disable-next-line no-console
+const error = () => console.error
+
+export default { assert, error, log, DEBUG }
